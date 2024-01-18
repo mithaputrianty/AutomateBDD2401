6f09e07f9b0af546e606fe369809ab1354707db9
mithaputrianty <putriantyf@gmail.com>
2024-01-18 08:36:42 +0700

    Update LoginMRS

---
+++ Object Repository/OpenMRS/loginPage/alertMessage.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>alertMessage</name>
+   <tag></tag>
+   <elementGuidId>f14af312-82ec-4cd3-80db-1242bbba80e9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'error-message']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>error-message</value>
+      <webElementGuid>f7660609-39a2-490f-b131-ce7409748dc0</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
